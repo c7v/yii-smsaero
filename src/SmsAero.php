9dbb563014c9ab6df93d7227dd1f028c814bbb49
Артём Соколовский <artem@sokolovsky.dev>
2022-02-06 21:16:15 +0400

    Init SmsAero.php and SmsAeroException.php

---
+++ src/SmsAero.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace c7v\yii_smsaero;
+
+use yii\base\Component;
+
+final class SmsAero extends Component
+{
+
+}
---
+++ src/SmsAeroException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace c7v\yii_smsaero;
+
+/**
+ * Class SmsAeroException
+ *
+ * @package c7v\yii_smsaero
+ * @author Artem Sokolovsky <artem@sokolovsky.dev>
+ */
+class SmsAeroException extends \Exception
+{
+
+}
